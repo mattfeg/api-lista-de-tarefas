8923c74137ab960952a1fe966d4e6e4a15134532
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-06-18 12:04:27 -0300

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.send("Olá mundo");
+});
+
+app.get('/boas-vindas', (req, res) => {
+    res.send(`Seja bem-vindo(a)`);
+});
+
+app.get('/boas-vindas/:nome', (req, res) => {
+    res.send(`Seja bem-vindo(a) ${req.params.nome}`);
+});
+
+app.get('/boas-vindas/:nome/:genero', (req, res) => {
+    res.send(`Seja bem-vindo(a) ${req.params.nome} do genero ${req.params.genero}`);
+});
+
+app.listen(port, () => {
+    console.log(`Serviço de pé: http://localhost:${port}`);
+})
