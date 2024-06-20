4b942a6179bf5574c01a8ba42ea86f64425e815e
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-06-20 12:13:24 -0300

    updates

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("Olá mundo.");
 });
@@ -18,6 +20,14 @@ app.get('/boas-vindas/:nome/:genero', (req, res) => {
     res.send(`Seja bem-vindo(a) ${req.params.nome} do genero ${req.params.genero}`);
 });
 
+app.post('/tarefas', (req, res) => {
+    res.send(JSON.stringify(req.body));
+})
+
+app.all("*", (req, res) => {
+    res.status(404).send("Página não encontrada");
+});
+
 app.listen(port, () => {
     console.log(`Serviço de pé: http://localhost:${port}`);
-})
+}) // colocar o teu comentario
