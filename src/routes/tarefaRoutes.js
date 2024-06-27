c2e58e9344a056cf8787b87f298d3ed83ac6383e
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-06-27 12:07:15 -0300

    updates

--- src/routes/tarefaRoutes.js
+++ src/routes/tarefaRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { executarSQL } = require("../database/index");
 
-router.get('/', (req, res) => {
-    res.send("listar tarefas");
+router.get('/', async (req, res) => {
+    res.send(await executarSQL("SELECT * FROM tarefas;"));
 });
-router.post('/', (req, res) => {
-    res.send("criar tarefas");
+router.post('/', async (req, res) => {
+    res.send(await executarSQL(`INSERT INTO tarefas (tarefa_titulo, tarefa_descricao) VALUES ("${req.body.tarefa_titulo}", "${req.body.tarefa_descricao}");`));
 });
 router.put('/:id', (req, res) => {
     res.send("editar tarefas");
